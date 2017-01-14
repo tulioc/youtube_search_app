833f868e6b331a5904d82799b6d1896128f9a59c
Tulio Coelho <tulioopc@gmail.com>
2017-01-14 02:15:08 -0200

    Adding onInputChange for searchbar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,10 +6,17 @@ class SearchBar extends Component {
     this.state = {term:''};
   }
 
+  onInputChange(term) {
+    this.setState({term});
+  }
+
   render() {
     return (
       <div className="search-bar">
-        <input value={this.state.term} />
+        <input
+        value={this.state.term}
+        onChange={event=> this.onInputChange(event.target.value)}
+        />
       </div>
     );
   }
