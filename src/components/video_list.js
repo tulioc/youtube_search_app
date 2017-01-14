719679287b0997876fbf675b6e0cd63449223b4e
Tulio Coelho <tulioopc@gmail.com>
2017-01-14 17:58:37 -0200

    Adding VideoListItem component

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import VideoListItem from './video_list_item.js'
+
 
 const VideoList = (props) => {
   return (
       <div className="col s4">
         <ul className="collection">
-          <li className="collection-item"> Video Item</li>
+          <VideoListItem />
         </ul>
       </div>
   );
---
+++ src/components/video_list_item.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const VideoListItem = () => {
+  return (
+    <li className="collection-item avatar">
+      <img src="" alt="" className="circle" />
+      <span className="title">Título</span>
+      <p>
+        Conteúdo
+      </p>
+    </li>
+  );
+}
+
+export default VideoListItem;
